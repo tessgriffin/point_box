4c5b045056d1109e94376aa2e9bedceb4061c95a
Tess Griffin <tessjgriffin@gmail.com>
2015-04-26 16:41:35 -0600

    Finished admin crud functionality for rewards

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,4 +1,5 @@
 class RewardsController < ApplicationController
+  before_action :ensure_admin_user, except: :index
   def index
     @rewards = Reward.all
   end
@@ -16,9 +17,33 @@ class RewardsController < ApplicationController
     end
   end
 
+  def edit
+    @reward = Reward.find(params[:id])
+  end
+
+  def update
+    @reward = Reward.find(params[:id])
+    if @reward.update(reward_params)
+      redirect_to rewards_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @reward = Reward.find(params[:id])
+    @reward.destroy
+    redirect_to rewards_path
+  end
   private
 
   def reward_params
     params.require(:reward).permit(:name, :description)
+  end
+
+  def ensure_admin_user
+    unless current_user.admin?
+      raise ActionController::RoutingError.new("Not found")
+    end
   end
 end
--- spec/features/user_views_rewards_index_spec.rb
+++ spec/features/user_views_rewards_index_spec.rb
@@ -44,4 +44,46 @@ feature 'User views rewards index' do
     visit rewards_path
     expect(page).to have_content("chocolate")
   end
+
+  scenario "admin can update a reward" do 
+    admin_user = User.create!(email: "admin@our_app.com", password: "secret", admin: true)
+    Reward.create!(name: "old", description: "old")
+
+    sign_in_as(admin_user)
+    visit rewards_path
+
+    click_on "Update reward"
+    within("form") do
+      fill_in "Name", with: "chocolate"
+      fill_in "Description", with: "yummy"
+    end
+    click_on "Submit"
+
+    visit rewards_path
+    expect(page).to have_content("chocolate")
+    expect(page).not_to have_content("old")
+  end
+
+  scenario "admin can delete a reward" do 
+    admin_user = User.create!(email: "admin@our_app.com", password: "secret", admin: true)
+    Reward.create!(name: "stuff", description: "super special description")
+
+    sign_in_as(admin_user)
+    visit rewards_path
+
+    click_on "Delete reward"
+
+    expect(page).not_to have_content("stuff")
+    expect(page).not_to have_content("super special description")
+  end
+
+  scenario "user cannot create rewards" do 
+    non_admin = User.create!(email: "stuff", password: "password", admin: false)
+
+    sign_in_as(non_admin)
+    expect {
+      visit new_reward_path(non_admin)
+    }.to raise_error(ActionController::RoutingError)
+
+  end
 end
